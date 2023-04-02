b7ec6653426795d1c9a6bb0933bd7b62cf2ea3f5
ICSRashmin <rashmingajera22nasa@gmail.com>
2023-04-02 22:45:53 +0530

    commit

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import OrderDetail, UserOrderList
-
-urlpatterns = [
-    path('orders/', UserOrderList.as_view()),
-    path('orders/<order_id>', OrderDetail.as_view()),
-]
--- cart/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import CartAPIView, CheckProductInCart
-
-urlpatterns = [
-    path('', CartAPIView.as_view()),
-    path('<product_id>/', CheckProductInCart.as_view()),
-]
